57c9a053aeb4a7048efdaa158f01e11d145ae06e
Chau Dinh <chaudvb95uc@gmail.com>
2019-09-18 20:25:41 +0700

    remove session and transfer models

--- server/models/session.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const SessionSchema = mongoose.Schema({
-  sessionId: String
-});
-
-const Session = mongoose.model("Session", SessionSchema, "sessions");
-
-module.exports = Session;
