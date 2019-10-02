a48dc4e731a134b124f718a4f5d36cb9e919dd8e
Chau Dinh <chaudvb95uc@gmail.com>
2019-10-02 15:26:03 +0700

    prettier

--- server/controllers/cart.controller.js
+++ server/controllers/cart.controller.js
@@ -1,10 +1,10 @@
-const Cart = require("../models/cart.model");
-const Product = require("../models/product.model");
+const Cart = require('../models/cart.model');
+const Product = require('../models/product.model');
 
 module.exports.portal = async (req, res) => {
-  let cart = await Cart.findOne({ owner: req.signedCookies.userId });
+  let cart = await Cart.findOne({owner: req.signedCookies.userId});
   if (!cart || cart.items.length === 0) {
-    res.redirect("/cart/empty");
+    res.redirect('/cart/empty');
   }
   let list = [...cart.items];
   let listName = list.map(item => item.productName);
@@ -12,33 +12,33 @@ module.exports.portal = async (req, res) => {
   for (let el of listName) {
     freqCounterName[el] = (freqCounterName[el] || 0) + 1;
   }
-  res.render("cart/portal", {
+  res.render('cart/portal', {
     cartNumber: Object.values(freqCounterName).reduce((a, b) => a + b),
-    freqCounterName
+    freqCounterName,
   });
 };
 
 module.exports.add = async (req, res) => {
   let productId = req.signedCookies.productId;
-  let cart = await Cart.findOne({ owner: req.signedCookies.userId });
+  let cart = await Cart.findOne({owner: req.signedCookies.userId});
   let itemsLength = cart ? cart.items.length : 0;
-  let product = await Product.findOne({ _id: productId });
+  let product = await Product.findOne({_id: productId});
 
   if (cart) {
     await Cart.findOneAndUpdate(
-      { _id: cart._id },
+      {_id: cart._id},
       {
         $push: {
           items: {
             productId: productId,
-            productName: product.name
-          }
+            productName: product.name,
+          },
         },
         $set: {
-          total: 9.99 * parseInt(itemsLength + 1)
-        }
+          total: 9.99 * parseInt(itemsLength + 1),
+        },
       },
-      { new: true }
+      {new: true},
     );
   } else {
     // create new document
@@ -46,67 +46,71 @@ module.exports.add = async (req, res) => {
       owner: req.signedCookies.userId,
       items: {
         productId: productId,
-        productName: product.name
+        productName: product.name,
       },
-      total: 9.99
+      total: 9.99,
     });
 
     await cart.save();
   }
-  res.redirect("/cart/portal");
+  res.redirect('/cart/portal');
 };
 
 module.exports.decrement = async (req, res) => {
-  let cart = await Cart.findOne({ owner: req.signedCookies.userId });
+  let cart = await Cart.findOne({owner: req.signedCookies.userId});
   let items = [...cart.items];
   let itemsName = items.map(item => item.productName);
   let index = itemsName.indexOf(Object.keys(req.body)[0]);
   items.splice(index, 1);
   await Cart.findOneAndUpdate(
-    { _id: cart._id },
+    {_id: cart._id},
     {
       $set: {
-        items: items
-      }
-    }
+        items: items,
+      },
+    },
   );
-  res.redirect("/cart/portal");
+  res.redirect('/cart/portal');
 };
 
 module.exports.increment = async (req, res) => {
   // let cart = await Cart.findOne({ owner: req.signedCookies.userId });
-  let product = await Product.findOne({ name: Object.keys(req.body)[0] });
+  let product = await Product.findOne({name: Object.keys(req.body)[0]});
   await Cart.findOneAndUpdate(
-    { owner: req.signedCookies.userId },
+    {owner: req.signedCookies.userId},
     {
       $push: {
         items: {
           productId: product._id,
-          productName: product.name
-        }
-      }
-    }
+          productName: product.name,
+        },
+      },
+    },
   );
-  res.redirect("/cart/portal");
+  res.redirect('/cart/portal');
 };
 
 module.exports.delete = async (req, res) => {
-  let cart = await Cart.findOne({ owner: req.signedCookies.userId });
+  let cart = await Cart.findOne({owner: req.signedCookies.userId});
   let items = [...cart.items];
   let removeItems = items.filter(
-    item => item.productName !== Object.keys(req.body)[0]
+    item => item.productName !== Object.keys(req.body)[0],
   );
   await Cart.findOneAndUpdate(
-    { owner: req.signedCookies.userId },
+    {owner: req.signedCookies.userId},
     {
       $set: {
-        items: removeItems
-      }
-    }
+        items: removeItems,
+      },
+    },
   );
-  res.redirect("/cart/portal");
+  res.redirect('/cart/portal');
 };
 
 module.exports.empty = (req, res) => {
-  res.render("cart/empty");
+  res.render('cart/empty');
+};
+
+const addNum = ({a, b}) => {
+  return a + b;
 };
